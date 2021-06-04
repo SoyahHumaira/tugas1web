80c2d32b18782fb41da14e989c4949f72697cbec
SoyahHumaira <soyahumaira04@gmail.com>
2021-06-04 17:39:15 +0700

    tugas1web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  const[sisi, setsisi] = useState("") 
+  const[hasil, sethasil] = useState("")
+
+  console.log(`sisi : ${sisi}`)
+
+  function luas(){
+    sethasil(6 * sisi * sisi)
+  }
+
+  return(
+    <div class = "App">
+      <p>
+        Masukan sisi
+      </p>
+      <input type = "text" value = {sisi} onChange = {e=> setsisi(+e.target.value)}></input>
+
+
+      <p>
+        <button onClick = {luas}>hasil</button>
+      </p>
+
+      <p>
+        {hasil}
+      </p>
+
     </div>
-  );
+  );  
 }
-
-export default App;
+export default App
